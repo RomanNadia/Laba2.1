ab603fff29f886d687a9389f102679b9623f706e
Nadia Roman <nad.roman14@gmail.com>
2020-06-01 23:47:28 +0300

    fourth commit

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace Lab2_05_01_
+namespace Lab2_05_02_
 {
     class Program
     {
         static void Input1(out int n)
         {
-            Console.Write("Enter number a i b =");
+            Console.Write("Enter n ");
             n = int.Parse(Console.ReadLine());
 
         }
@@ -14,20 +14,21 @@ namespace Lab2_05_01_
         {
             a = new int[n, n];
             b = new int[n, n];
+            int min = -100, max = 100;
+            Random rnd = new Random();
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < n; j++)
                 {
-                    Console.Write("Enter elemets of array a[{0}][{1}] = ", i, j);
-                    a[i, j] = int.Parse(Console.ReadLine());
+                    a[i, j] = rnd.Next(min, max + 1);
                 }
             }
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < n; j++)
                 {
-                    Console.Write("Enter elemets of array b[{0}][{1}] = ", i, j);
-                    b[i, j] = int.Parse(Console.ReadLine());
+
+                    b[i, j] = rnd.Next(min, max + 1);
                 }
             }
         }
@@ -77,16 +78,15 @@ namespace Lab2_05_01_
             int[,] b = new int[n, n];
 
             Input2(n, out a, out b);
-            Console.WriteLine("\n array A with nxn elements : ");
+            Console.WriteLine("\n array A with nxn elements: ");
             Output(n, a);
 
-            Console.WriteLine("\n array B with nxn elements : ");
+            Console.WriteLine("\n array B with nxn elements: ");
             Output(n, b);
-            Console.WriteLine("\n new array");
+            Console.WriteLine("\n new array ");
             Change(ref a, b, n);
             Output(n, a);
 
         }
     }
 }
-
